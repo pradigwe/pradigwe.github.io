356a8bfe75d3ec5efaaad05f904d17188297eab4
Precious Adigwe <padigwe@charlotte.edu>
2025-03-18 14:16:39 -0400

    fix validation icons & secondary menu

--- itis3135/scripts/validation-icons.js
+++ itis3135/scripts/validation-icons.js
@@ -1,2 +1,9 @@
-document.getElementById("validation-link-html").setAttribute("href", "https://validator.w3.org/check?uri=" + location.href);
-document.getElementById("validation-link-css").setAttribute("href", "https://jigsaw.w3.org/css-validator/validator?uri=" + location.href);
+const html = document.getElementById("validation-link-html");
+const css = document.getElementById("validation-link-css");
+const wave = document.getElementById("validation-link-wave");
+
+html.setAttribute("href", "https://validator.w3.org/check?uri=" + location.href);
+css.setAttribute("href", "https://jigsaw.w3.org/css-validator/validator?uri=" + location.href);
+if (wave !== null && wave !== "") {
+    wave.setAttribute("href", "https://wave.webaim.org/report#/" + location.href);
+}
